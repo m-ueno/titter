8a3e790afd29f472a77eb79d6d32151cc6868c90
uenop <dummy@mail.com>
2011-10-04 21:52:30 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Titter::Application.config.secret_token = '78388162632ca8a47b2c19f5cbeb0759851045978556f160b403af670fb3ab4d8515fa32ea830adc14b28e18ab51a7405f0bda66e99caaed2bdf42eca9e4a1d4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Titter::Application.config.session_store :cookie_store, :key => '_titter_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Titter::Application.config.session_store :active_record_store
